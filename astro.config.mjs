737e05506b09878f8aecf7ee15fec723658dd4fd
blackbird014 <andrehmerli@gmail.com>
2025-03-02 13:28:30 +0100

    fix: update build config to handle assets correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blackbird014.github.io',
-  base: isProd ? '/my-cv-site' : '',
+  base: '/my-cv-site',
+  outDir: './dist',
+  build: {
+    format: 'file'
+  }
 });
