12bb009a7d71b8cb5df08fad5353b12bd0dfd35d
blackbird014 <andrehmerli@gmail.com>
2025-03-02 18:30:00 +0100

    fix: update build config with format: file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blackbird014.github.io',
-  base: '/my-cv-site'
+  base: '/my-cv-site',
+  outDir: './dist',
+  build: {
+    format: 'file'
+  }
 });
